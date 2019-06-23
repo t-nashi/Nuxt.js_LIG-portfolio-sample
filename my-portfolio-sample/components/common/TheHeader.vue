ae5cb545cc7c535b83135839332176c4afa9a530
TsutomuTakanashi <t_19nashi@yahoo.co.jp>
2019-06-23 12:04:42 +0900

    add

---
+++ my-portfolio-sample/components/common/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+<header class="header">
+<div class="header-inner">
+    <h1>my-portfolio</h1>
+    自己紹介
+  </div>
+</header>
+</template>
+<style scoped lang="scss">
+  // @nuxtjs/style-resourcesプラグインインストール後にnuxt.config.js|modules・styleResources内に記述したことにより不要になった
+  // @import "~assets/scss/variables";
+
+  .header {
+    background-color: $color-gray;
+  }
+
+  .header-inner {
+    height: 60px;
+    max-width: 600px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    a {
+      color: inherit;
+    }
+  }
+</style>
